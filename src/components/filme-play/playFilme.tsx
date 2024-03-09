62356f6c824ab889661c87ced90d2d1f69ad54fc
Ivo Ryan <ryanbussines06@gmail.com>
2024-03-09 19:03:48 -0300

    Update

--- src/components/filme-play/playFilme.tsx
+++ src/components/filme-play/playFilme.tsx
@@ -1,5 +1,5 @@
 import * as S from "./style";
-// import  PosterImg  from '../../assets/poster.jpg';
+import  PosterImg  from '../../assets/poster.jpg';
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -9,7 +9,7 @@ export const FilmePlay = () => {
     const { id } = useParams();
 
     const [ filme, setFilme ] = useState<string>('');
-    const [ url , setUrl ] = useState<string>('')
+    const [ url , setUrl ] = useState<string>('');
     const [ descript, setDescript ] = useState<string>('');
 
     useEffect(() => {
@@ -36,7 +36,7 @@ export const FilmePlay = () => {
             <meta itemProp="thumbnailUrl" content={url}/>
             
             <div className="container-video">
-                <video controls autoPlay  src="https://ns5019087.ip-15-235-118.net/upmp4/animes/s/solo-leveling/01.mp4" > 
+                <video controls autoPlay poster={PosterImg} src={url}>
                     </video>
             </div> 
             
