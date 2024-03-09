376408a69715fc8ff2bac3894afd0a16ebf4f49d
Ivo Ryan <ryanbussines06@gmail.com>
2024-03-09 17:41:41 -0300

    Update

--- src/components/filme-play/playFilme.tsx
+++ src/components/filme-play/playFilme.tsx
@@ -39,6 +39,10 @@ export const FilmePlay = () => {
                 <video controls autoPlay poster={PosterImg} src={url}>
                      </video>
             </div>
+
+            <div>
+                <video src="https://rr3---sn-jvhhjvg2-cnce.googlevideo.com/videoplayback?expire=1710043345&ei=UcDsZYWXCqmuy_sP5P6GCA&ip=45.188.52.225&id=73b9ba8a0a0cbbc8&itag=22&source=blogger&xpc=Egho7Zf3LnoBAQ%3D%3D&mh=1U&mm=31&mn=sn-jvhhjvg2-cnce&ms=au&mv=m&mvi=3&pl=24&susc=bl&eaua=Aaz2Nyfyfhw&mime=video/mp4&vprv=1&dur=1420.202&lmt=1710007678221035&mt=1710013754&txp=1311224&sparams=expire,ei,ip,id,itag,source,xpc,susc,eaua,mime,vprv,dur,lmt&sig=AJfQdSswRQIhAMlv58dhoAGMW0e0SGp9-FAf0T7SlujdyQwM3Yi29ALkAiAdiWXoxaec6-DhV9rW_QXy6WDgRcmr85wjGm_jDB93qA%3D%3D&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=APTiJQcwRgIhANN8BzymbAO1Cm5MqBjOwR3cyiCa1iDYsZaJakpVH6LDAiEAoEiO-IzeJFrKsMCsX-EJfadIlUTCGUyqpZAlqViKJfs%3D&cpn=_cycXvo9ibt7Cxv4&c=WEB_EMBEDDED_PLAYER&cver=1.20240305.00.00" controls autoPlay></video>
+            </div>
             
         </S.PlayInVideo>
 
