881c6590b9a69a927cf7944ea926df1de1fdc452
Ivo Ryan <ryanbussines06@gmail.com>
2024-04-24 21:27:56 -0300

    Update

--- src/pages/dashboard/style.tsx
+++ src/pages/dashboard/style.tsx
@@ -1,5 +1,16 @@
 import { styled } from 'styled-components';
+import poster from '../../assets/preview.jpg';
+
 
 export const DashboardContainer = styled.main`
         width: 100%;
+        height: 100vh;
+        background-image: url(${poster});
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-attachment: fixed;
+
+        @media (max-width: 768px) {
+            background-position: 100%;
+        }
 `;
